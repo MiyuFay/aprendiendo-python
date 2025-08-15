fa06e9a795a77fa10f25d75a21e910a79faa4904
MiyuFay <ayamiyu001@gmail.com>
2025-08-15 17:08:14 +0200

    Add files via upload
    Añadido ejercicio 10 de variables: mostrar nombre dos veces con exclamaciones

---
+++ variables/ejercicio_variables_10.py
@@ -0,0 +1,7 @@
+# Ejercicio 10: Pedir el nombre del usuario y mostrarlo dos veces en una sola línea con signos de exclamación
+
+# Pedimos al usuario que ingrese su nombre
+nombre = input("¿Cuál es tu nombre?: ")
+
+# Mostramos el nombre dos veces en la misma línea, con exclamaciones al inicio, entre los nombres y al final
+print(f'!{nombre}!{nombre}!')
