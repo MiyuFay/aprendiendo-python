6a95d79854cea48c5746af20dbcc1e68950eeda0
MiyuFay <ayamiyu001@gmail.com>
2025-08-15 16:58:58 +0200

    Add files via upload
    Añadido ejercicio 5 de variables: imprimir nombres de personajes en orden alfabético

---
+++ variables/ejercicio_variables_5.py
@@ -0,0 +1,10 @@
+# Ejercicio 5: Imprimir nombres de personajes en orden alfabético
+
+# Nombres de los personajes
+print("Draco")
+print("Ginny")
+print("Harry")
+print("Hermione")
+print("Luna")
+print("Neville")
+print("Ron")
