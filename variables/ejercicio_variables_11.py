6a96d7f8687f568425132a7ad2aceb06b69c3764
MiyuFay <ayamiyu001@gmail.com>
2025-08-15 17:16:45 +0200

    Add files via upload
    Añadido ejercicio 11 de variables: pedir nombre y dirección y mostrarla

---
+++ variables/ejercicio_variables_11.py
@@ -0,0 +1,13 @@
+# Ejercicio 11: Pedir al usuario su nombre y dirección, y mostrar la información
+
+# Pedimos al usuario los datos
+nombre = input("Nombre: ")
+apellido = input("Apellido: ")
+direccion = input("Dirección: ")
+ciudad_codigo = input("Ciudad y código postal: ")
+
+# Mostramos la información completa en líneas separadas
+print(nombre + " " + apellido)
+print(direccion)
+print(ciudad_codigo)
+
