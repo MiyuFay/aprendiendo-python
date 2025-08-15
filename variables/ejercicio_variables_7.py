ffd0645a6a3a96549609619b88b8459cfaa39c26
MiyuFay <ayamiyu001@gmail.com>
2025-08-15 17:01:55 +0200

    Add files via upload
    Añadido ejercicio 7 de variables: calcular minutos en un año

---
+++ variables/ejercicio_variables_7.py
@@ -0,0 +1,17 @@
+# Ejercicio 7: Calcular el número de minutos en un año
+
+# Definimos los días del año
+dias_anio = 365
+
+# Definimos las horas por día y minutos por hora
+horas_dia = 24
+minutos_hora = 60
+
+# Calculamos los minutos en un día
+minutos_dia = horas_dia * minutos_hora
+
+# Calculamos los minutos en un año
+minutos_anio = dias_anio * minutos_dia
+
+# Mostramos el resultado
+print(minutos_anio)
