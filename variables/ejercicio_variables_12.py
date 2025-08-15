d7b96251a452cd4c30c4f1be3e6f6014e185eba1
MiyuFay <ayamiyu001@gmail.com>
2025-08-15 17:18:23 +0200

    Add files via upload
    Añadido ejercicio 12 de variables: concatenar tres partes de una rima

---
+++ variables/ejercicio_variables_12.py
@@ -0,0 +1,9 @@
+# Ejercicio 12: Pedir tres partes de una rima y mostrarlas unidas con guiones
+
+# Pedimos al usuario las tres partes
+parte_1 = input("La 1ª parte: ")
+parte_2 = input("La 2ª parte: ")
+parte_3 = input("La 3ª parte: ")
+
+# Mostramos las tres partes unidas con guiones y un signo de exclamación al final
+print(parte_1 + '-' + parte_2 + '-' + parte_3 + '!')
