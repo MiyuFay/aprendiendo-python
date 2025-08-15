be41623133d0556483a1f5c7d56da72826cc6ddd
MiyuFay <ayamiyu001@gmail.com>
2025-08-15 17:00:38 +0200

    Add files via upload
    Añadido ejercicio 6 de variables: imprimir canción infantil en varias líneas

---
+++ variables/ejercicio_variables_6.py
@@ -0,0 +1,7 @@
+# Ejercicio 6: Imprimir varias líneas de una canción en consola
+
+print('''Estrellita, ¿dónde estás?
+Quisiera verte brillar.
+En el cielo o en el mar,
+Un deseo te voy a dar.
+''')
