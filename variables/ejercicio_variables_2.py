eaff9eb8c5a4fbd16f711a9cb766fba2ab273c70
MiyuFay <ayamiyu001@gmail.com>
2025-08-15 16:46:35 +0200

    Add files via upload
    Añadido ejercicio 2 de variables: buenas prácticas en nombres

---
+++ variables/ejercicio_variables_2.py
@@ -0,0 +1,14 @@
+# Ejercicio 2: Buenas prácticas en nombres de variables
+
+# Variables con nombres descriptivos
+# Nombres claros ayudan a que el código sea más fácil de leer
+nombre_completo = "Ana Sanchez"
+edad_usuario = 30
+correo_electronico = "ana@mail.com"
+saldo_cuenta = 1000.50
+
+# Evitar usar nombres cortos o de una sola letra
+contador = 5
+
+# Evitar palabras reservadas de Python (klass se usa en lugar de class)
+klass = 10
