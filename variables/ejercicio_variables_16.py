9bfd6e41382895810cac73c44eefab104ff6b0c3
MiyuFay <ayamiyu001@gmail.com>
2025-08-15 17:30:59 +0200

    Añadido ejercicio 16 de variables: mostrar cálculo en una sola línea

---
+++ variables/ejercicio_variables_16.py
@@ -0,0 +1,10 @@
+# Ejercicio 16: Mostrar un cálculo en una sola línea usando end=""
+
+# Mostramos cada parte del cálculo sin salto de línea
+print(7, end="")
+print(" + ", end="")
+print(3, end="")
+print(" - ", end="")
+print(2, end="")
+print(" = ", end="")
+print(7 + 3 - 2, end="")
