130e6cbd70cc0d135caf9b43685d8208a55bb13a
MiyuFay <ayamiyu001@gmail.com>
2025-08-15 16:43:46 +0200

    Add files via upload
    Añadido ejercicio 1 de variables: definir e imprimir

---
+++ variables/ejercicio_variables_1.py
@@ -0,0 +1,11 @@
+# Ejercicio 1: Definir variables y mostrarlas en consola
+
+# Definimos variables con diferentes tipos de datos
+nombre = 'Marina'       # String
+edad = 28             # Entero
+pais = 'España'       # String
+
+# Mostramos los valores en la consola
+print('Nombre: ', nombre)
+print('Edad: ', edad)
+print('País: ', pais)
