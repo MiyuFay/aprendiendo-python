69680d686c0412349c38310ff0ec41ac9cb37d3d
MiyuFay <ayamiyu001@gmail.com>
2025-08-15 17:27:31 +0200

    Añadido ejercicio 15 de variables: operaciones básicas con dos números

---
+++ variables/ejercicio_variables_15.py
@@ -0,0 +1,11 @@
+# Ejercicio 15: Operaciones básicas con dos números
+
+# Definimos los números
+x = 10
+y = 5
+
+# Mostramos las operaciones básicas: suma, resta, multiplicación y división
+print(f'{x} + {y} = {x + y}')
+print(f'{x} - {y} = {x - y}')
+print(f'{x} * {y} = {x * y}')
+print(f'{x} / {y} = {x / y}\n')
