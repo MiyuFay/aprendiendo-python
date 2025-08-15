b566a775db3db3642b6d938cc79677e33edec9cc
MiyuFay <ayamiyu001@gmail.com>
2025-08-15 17:05:03 +0200

    Add files via upload
    Añadido ejercicio 9 de variables: pedir nombre y mostrarlo dos veces

---
+++ variables/ejercicio_variables_9.py
@@ -0,0 +1,8 @@
+# Ejercicio 9: Pedir el nombre del usuario y mostrarlo dos veces
+
+# Pedimos al usuario que ingrese su nombre
+nombre = input("¿Cuál es tu nombre?: ")
+
+# Mostramos el nombre dos veces, en líneas consecutivas
+print(nombre)
+print(nombre)
