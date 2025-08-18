758b0c64ac39fb7a4c4f6158dc57585c1e50b7e4
MiyuFay <ayamiyu001@gmail.com>
2025-08-18 16:47:09 +0200

    Añadido ejercicio 20 de variables: calcular el producto de tres números

---
+++ variables/ejercicio_variables_20.py
@@ -0,0 +1,13 @@
+# Ejercicio 20: Pedir tres números al usuario
+# y mostrar el producto de los tres números
+
+# Pedimos al usuario los tres números (convertidos a entero)
+numero1 = int(input("Escribe el primer número: "))
+numero2 = int(input("Escribe el segundo número: "))
+numero3 = int(input("Escribe el tercer número: "))
+
+# Calculamos el producto de los tres números
+producto = numero1 * numero2 * numero3
+
+# Mostramos el resultado en pantalla
+print(f"El producto es: {producto}")
