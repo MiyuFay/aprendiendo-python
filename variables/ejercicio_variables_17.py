03f03afed8d3cfb51c70ede8f42c5700c92d9bd5
MiyuFay <ayamiyu001@gmail.com>
2025-08-18 16:35:01 +0200

    Añadido ejercicio 17 de variables: multiplicar un número por 5

---
+++ variables/ejercicio_variables_17.py
@@ -0,0 +1,10 @@
+# Ejercicio 17 (variables): Multiplicar un número por 5
+
+# Pedimos al usuario que escriba un número
+number = int(input("Please type in a number: "))
+
+# Multiplicamos ese número por 5
+x = number * 5
+
+# Mostramos el resultado en pantalla
+print(f"{number} times 5 is {x}")
