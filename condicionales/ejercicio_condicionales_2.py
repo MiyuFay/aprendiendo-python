85684164922884f825120bdaf674ccd7eeaa2cde
MiyuFay <ayamiyu001@gmail.com>
2025-08-18 17:16:48 +0200

    Añadido ejercicio 2 de condicionales: mostrar autor según año de publicación

---
+++ condicionales/ejercicio_condicionales_2.py
@@ -0,0 +1,17 @@
+# Ejercicio 2: Comprobar un año de publicación y mostrar el autor de la novela
+
+# Este ejercicio se ha adaptado de ejercicios disponibles en la web:
+# https://programming-23.mooc.fi/part-1/5-conditional-statements
+
+# Pedimos al usuario que escriba un número entero
+anio = int(input("Escribe un año: "))
+
+# Si el año coincide con la publicación de la novela, mostramos el autor
+if anio == 1813:
+    print("Jane Austen")
+
+# Ejemplo de ejecución:
+# Escribe un año: 2020
+# (no muestra nada)
+# Escribe un año: 1813
+# Jane Austen
